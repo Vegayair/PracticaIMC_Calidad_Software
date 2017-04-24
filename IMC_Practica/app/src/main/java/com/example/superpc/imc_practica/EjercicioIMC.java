50edcfe1b1d7a6624264f4c56cc85e5dae6c2a1b
Vegayair <vegayair@hotmail.com>
2017-04-24 01:25:59 -0500

    Codificación
    Creacion de variables y Metodo limpiar

--- IMC_Practica/app/src/main/java/com/example/superpc/imc_practica/EjercicioIMC.java
+++ IMC_Practica/app/src/main/java/com/example/superpc/imc_practica/EjercicioIMC.java
@@ -2,12 +2,32 @@ package com.example.superpc.imc_practica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class EjercicioIMC extends AppCompatActivity {
 
+    EditText peso, altura;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio_imc);
+
+        Button calcular = (Button) findViewById(R.id.Calcular);
+        Button salir = (Button) findViewById(R.id.Salir);
+
+
+        peso = (EditText) findViewById(R.id.Peso);
+        altura = (EditText) findViewById(R.id.Altura);
+
     }
+
+    public void limpiar(){
+        peso.setText("");
+        altura.setText("");
+    }
+
 }
+
+
