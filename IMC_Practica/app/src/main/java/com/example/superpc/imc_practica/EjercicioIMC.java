dec721d51286350edf21a6cc073fe09a7e1ec3e7
Vegayair <vegayair@hotmail.com>
2017-04-24 00:06:56 -0500

    Creación del proyecto
    Se crea el proyecto en android studio

---
+++ IMC_Practica/app/src/main/java/com/example/superpc/imc_practica/EjercicioIMC.java
@@ -0,0 +1,13 @@
+package com.example.superpc.imc_practica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EjercicioIMC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio_imc);
+    }
+}
