4683c35596e64aa14bd5e21ca177923926e66f80
Vegayair <vegayair@hotmail.com>
2017-04-24 02:12:04 -0500

    Codificación
    Codificacion de la aplicacion

--- IMC_Practica/app/src/main/java/com/example/superpc/imc_practica/EjercicioIMC.java
+++ IMC_Practica/app/src/main/java/com/example/superpc/imc_practica/EjercicioIMC.java
@@ -2,11 +2,13 @@ package com.example.superpc.imc_practica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class EjercicioIMC extends AppCompatActivity {
+public class EjercicioIMC extends AppCompatActivity implements View.OnClickListener{
 
     EditText peso, altura;
     @Override
@@ -17,12 +19,80 @@ public class EjercicioIMC extends AppCompatActivity {
         Button calcular = (Button) findViewById(R.id.Calcular);
         Button salir = (Button) findViewById(R.id.Salir);
 
+        Button salir = (Button) findViewById(R.id.Salir);
+        salir.setOnClickListener(this);
 
         peso = (EditText) findViewById(R.id.Peso);
         altura = (EditText) findViewById(R.id.Altura);
 
     }
 
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.Salir:
+                System.exit(0);
+                break;
+            case R.id.Calcular:
+                if (peso.getText().toString().trim().length() == 0) {
+                    peso.requestFocus();
+                    Toast.makeText(EjercicioIMC.this,
+                            "El campo peso esta vacio", Toast.LENGTH_SHORT)
+                            .show();
+                } else if (altura.getText().toString().trim().length() == 0) {
+                    altura.requestFocus();
+                    Toast.makeText(EjercicioIMC.this,
+                            "El campo altura esta vacio",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    CalcuIMC();
+                    break;
+                }
+        }
+    }
+    public void CalcuIMC(){
+
+        IMC calcu = new IMC();
+        float kg=Float.parseFloat(peso.getText().toString());
+        float cm=Float.parseFloat(altura.getText().toString());
+        calcu.calcula(kg,cm);
+        float m = (cm / 100);
+        float bmi = (kg / (m * m));
+        if (bmi < 18.5) {
+            Toast.makeText(EjercicioIMC.this,
+                    "Tu IMC es " + bmi + "\n" + "Estas delgado",
+                    Toast.LENGTH_SHORT).show();
+            limpiar();
+        } else {
+            if (bmi < 24.9) {
+                Toast.makeText(
+                        EjercicioIMC.this,
+                        "Tu IMC es " + bmi + "\n"
+                                + "Tu peso es normal",
+                        Toast.LENGTH_SHORT).show();
+                limpiar();
+            } else {
+                if (bmi < 29.9) {
+                    Toast.makeText(
+                            EjercicioIMC.this,
+                            "Tu IMC es " + bmi + "\n"
+                                    + "Tienes sobrepeso",
+                            Toast.LENGTH_SHORT).show();
+                    limpiar();
+
+                } else {
+                    if (bmi > 30) {
+                        Toast.makeText(
+                                EjercicioIMC.this,
+                                "Tu IMC es " + bmi + "\n"
+                                        + "Padeces obesidad",
+                                Toast.LENGTH_SHORT).show();
+                        limpiar();
+                    }
+                }
+            }
+        }
+    }
     public void limpiar(){
         peso.setText("");
         altura.setText("");
